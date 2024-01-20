2ab4361982d1d777074546780b8946d41a36fdcf
EricDeBuitleir <72872237+EricDeBuitleir@users.noreply.github.com>
2024-01-20 14:03:30 +0000

    making all fyp files public

---
+++ Final Year Project/waterfordMapTest.js
@@ -0,0 +1,47 @@
+/**
+ * Moves the map to display over Waterford
+ *
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
+
+
+function moveMapToWaterford(map){
+    map.setCenter({lat:52.259988, lng: -7.111880});
+    map.setZoom(14);
+  }
+  
+
+
+  /**
+   * Boilerplate map initialization code starts below:
+   */
+  
+  //Step 1: initialize communication with the platform
+  // In your own code, replace variable window.apikey with your own apikey
+  var platform = new H.service.Platform({
+    apikey: PiGIPzWO6d9vRa7vd0dieYO7dlGnHM3O6qFc2KPohK8
+  });
+  var defaultLayers = platform.createDefaultLayers();
+  
+  //Step 2: initialize a map - this map is centered over Europe
+  var map = new H.Map(document.getElementById('map'),
+    defaultLayers.vector.normal.map,{
+    center: {lat:50, lng:5},
+    zoom: 4,
+    pixelRatio: window.devicePixelRatio || 1
+  });
+  // add a resize listener to make sure that the map occupies the whole container
+  window.addEventListener('resize', () => map.getViewPort().resize());
+  
+  //Step 3: make the map interactive
+  // MapEvents enables the event system
+  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+  
+  // Create the default UI components
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+  
+  // Now use the map as required...
+  window.onload = function () {
+    moveMapToWaterford(map);
+  }
